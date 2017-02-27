baf3cbe67125a55b3efd6a416f7a3c28941f279e
Grégoire <greg.joncour@gmail.com>
2017-02-27 15:41:02 +0100

    redirection map

--- app/src/main/java/com/example/allancontaret/restory/WelcomeActivity.java
+++ app/src/main/java/com/example/allancontaret/restory/WelcomeActivity.java
@@ -1,7 +1,10 @@
 package com.example.allancontaret.restory;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,6 +12,17 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button fab = (Button) findViewById(R.id.enter_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+                Intent intent = new Intent(WelcomeActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
