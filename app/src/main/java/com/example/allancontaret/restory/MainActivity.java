5ffcb966b52425c52b063b48db731d292ce25437
Allan-Contaret <allancontaret@MacBook-Pro-de-Allan.local>
2017-02-27 11:04:40 +0100

    Début de l'appli

---
+++ app/src/main/java/com/example/allancontaret/restory/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.allancontaret.restory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ImageView img = (ImageView) findViewById(R.id.pageGarde);
+        img.setBackgroundResource(R.drawable.restaurant);
+    }
+}
